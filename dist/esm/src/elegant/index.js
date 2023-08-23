b217e89ef1276a4232f42e0f1c7233d07ed57dc5
Nicat-dcw <supradcwnicat@gmail.com>
2023-08-23 23:14:28 +0400

    hotfix
    .exit
    git commit -m hotfix

--- dist/esm/src/elegant/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.JSONAdaptor = exports.ElegantAdaptor = void 0;
-const json_1 = require("./adaptors/json");
-Object.defineProperty(exports, "JSONAdaptor", { enumerable: true, get: function () { return json_1.JSONAdaptor; } });
-const elegant_1 = require("./adaptors/elegant");
-Object.defineProperty(exports, "ElegantAdaptor", { enumerable: true, get: function () { return elegant_1.ElegantAdaptor; } });
